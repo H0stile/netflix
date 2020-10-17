5a5cb33082de4fa24cb0845d5f67d67ecb416d72
CharlesWilmart <chwilmart@gmail.com>
2020-10-17 17:19:41 +0200

    Creating the jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Jumbotron from './components/jumbotron'
+import jumboData from './fixtures/jumbo.json'
 
 
 export default function App() {
   return (
-    <Jumbotron />
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <Jumbotron.pane> 
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.pane>
+          <Jumbotron.pane> 
+            <Jumbotron.Image src={item.image} alt={item.alt}></Jumbotron.Image>
+           </Jumbotron.pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
 
--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,6 +1,26 @@
 import React from 'react';
-import { Inner } from './styles/jumbotron'
+import { Inner, Container } from './styles/jumbotron'
 
-export default function Jumbotron({}){
-    return (<Inner>Hello Again !</Inner>)
+export default function Jumbotron({children, direction = 'row', ...restProps}){     //  Props = direction, if there is prop given, default will be "row"
+    return (
+    <Inner direction={direction}>
+        {children}
+    </Inner>)
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }){
+    return <Container {...restProps}>{children}</Container>;
+}
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }){
+    return <Pane {...restProps}>{children}</Pane>;
+}
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }){
+    return <Title {...restProps}>{children}</Title>;
+}
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }){
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+}
+Jumbotron.Image = function JumbotronImage({ children, ...restProps }){
+    return <Image {...restProps}>{children}</Image>;
 }
