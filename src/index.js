d57ce79caed0bb7ba67bf5d1aa613531b45853bc
CharlesWilmart <chwilmart@gmail.com>
2020-10-19 22:40:05 +0200

    Revert "Revert "finished the jumbotron styling""
    This reverts commit 04355f4a0005778cc46adb1086f768625b0f4d38.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './app';
+import { GlobalStyles } from './global-styles'
 
 
 render(
-  <App />,
+  // <> this is a fragment
+  <>  
+    <GlobalStyles />
+    <App />
+  </>,
   document.getElementById('root')
 );
